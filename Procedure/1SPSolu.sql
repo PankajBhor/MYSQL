756b62207ba91978332975958735f8f70b035be7
Pankaj Bhor <pankajbhor373@gmail.com>
2025-07-25 10:16:22 +0530

    1st Problem Solved

---
+++ Procedure/1SPSolu.sql
@@ -0,0 +1,10 @@
+
+-- 1. Creating a Stored Procedure for User Registration
+
+DELIMITER //
+CREATE PROCEDURE puserresgistration(IN uname VARCHAR(50), IN upassword VARCHAR(255), IN uemail VARCHAR(100), IN uaddress VARCHAR(255))
+BEGIN
+	INSERT INTO users(username, password, email, address) VALUES(uname,upassword,uemail,uaddress);
+END //
+
+CALL  puserresgistration('Pankaj','PankajBhor','pankaj@email.com','awsari kd ,pune');
